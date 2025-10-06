fa9e3b382f2a7dd8bf763f812a6a12b0a033bc8d
mohammad <mohammad@genovoai.com>
2025-10-06 09:44:20 +0330

    run python script

--- script.py
+++ script.py
@@ -1,5 +1,22 @@
+import boto3
 
+# Create an EC2 resource object (replace 'your-region' with your AWS region)
+ec2 = boto3.resource('ec2', region_name='us-west-1')
 
+# Iterate through all instances
+for instance in ec2.instances.all():
+    print(f"Instance ID: {instance.id}")
+    print(f"Instance Type: {instance.instance_type}")
+    print(f"State: {instance.state['Name']}")
+    print(f"Public IP: {instance.public_ip_address}")
+    # You can access other attributes like tags, launch time, etc.
+    for tag in instance.tags or []:  # Handle cases where instances might not have tags
+        if tag['Key'] == 'Name':
+            print(f"Name: {tag['Value']}")
+    print("-" * 20)
 
-for i in range(1, 10):
-    print(i)
+# To filter instances (e.g., by state)
+for instance in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]):
+    print(f"Running Instance ID: {instance.id}")
+    print(f"Running Instance Type: {instance.instance_type}")
+    print("-" * 20)
